7dd5c89ab5eaa0469c756b668be7a872e9326525
Felipe S Bezerra <felipecrf220@gmail.com>
2023-05-17 22:08:51 -0300

    implementado animacao da sidenav

--- src/app/sidenav/nav-data.ts
+++ src/app/sidenav/nav-data.ts
@@ -3,5 +3,35 @@ export const navbarData = [
         routerLink: 'dashboard',
         icon: 'fal fa-home',
         label: 'Dashboard'
+    },
+    {
+        routerLink: 'products',
+        icon: 'fal fa-box-open',
+        label: 'Produtos'
+    },
+    {
+        routerLink: 'statistics',
+        icon: 'fal fa-chart-bar',
+        label: 'Estatísticas'
+    },
+    {
+        routerLink: 'coupens',
+        icon: 'fal fa-tag',
+        label: 'Cupons'
+    },
+    {
+        routerLink: 'pages',
+        icon: 'fal fa-file',
+        label: 'Páginas'
+    },
+    {
+        routerLink: 'media',
+        icon: 'fal fa-camera',
+        label: 'Mídias'
+    },
+    {
+        routerLink: 'settings',
+        icon: 'fal fa-cog',
+        label: 'Configurações'
     }
 ];
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { navbarData } from './nav-data';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
 
-  collapsed = true;
+  @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
+  collapsed = false;
+  screenWidth = 0;
   navData = navbarData;
+
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+  }
+
+  toggleColapsed(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+  }
+
+  closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+  }
 }
